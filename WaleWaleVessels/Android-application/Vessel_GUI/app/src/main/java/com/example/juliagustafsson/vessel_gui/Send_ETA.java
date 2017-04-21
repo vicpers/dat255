d8ecac502ec81c27e3fe48d6f55a4c99bb97eaf1
ericjosefsson <ericjosefsson@Erics-MacBook-Pro.local>
2017-04-21 15:12:47 +0200

    Skicka ETA - Validering på datum och tid

--- WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/Send_ETA.java
+++ WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/Send_ETA.java
@@ -1,8 +1,14 @@
 package com.example.juliagustafsson.vessel_gui;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Send_ETA extends AppCompatActivity {
 
@@ -15,7 +21,46 @@ public class Send_ETA extends AppCompatActivity {
     public void sendNewETA(View view) {
         //TODO Kolla inputs så att de innehåller giltiga värden (Datum och tid)
         //TODO kod för att uppdatera senast skickat ETA
-        //update_str = Context.getResources().getString (R.string.update_identifier)
-        //Update the label using ((TextView)findViewById (R.id.textview)).setText (updated_str)
+
+        EditText dateText = (EditText) findViewById(R.id.editText2);
+        EditText timeText = (EditText) findViewById(R.id.editText);
+        String date = dateText.getText().toString();
+        String time = timeText.getText().toString();
+
+
+        if(!isValidDate(date)){
+            dateText.setText("Invalid date");
+            dateText.setTextColor(Color.RED);
+        }
+        if(!isValidTime(time)){
+            timeText.setText("Invalid time");
+            timeText.setTextColor(Color.RED);
+        }
     }
+
+
+
+
+    public static boolean isValidDate(String date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        simpleDateFormat.setLenient(false);
+        try {
+            simpleDateFormat.parse(date.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static boolean isValidTime(String time) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        simpleDateFormat.setLenient(false);
+        try {
+            simpleDateFormat.parse(time.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+
 }

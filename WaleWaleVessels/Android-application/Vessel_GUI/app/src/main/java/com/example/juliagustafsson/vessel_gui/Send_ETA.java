0472ac62102132ce1f5ea67e9bfa1e82de0240b3
ericjosefsson <ericjosefsson@Erics-MBP.lan>
2017-04-22 14:09:46 +0200

    Smidigare sätt att välja tid och datum

--- WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/Send_ETA.java
+++ WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/Send_ETA.java
@@ -1,66 +1,78 @@
 package com.example.juliagustafsson.vessel_gui;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-public class Send_ETA extends AppCompatActivity {
+import HTTPRequest.PCM;
+
+import static android.R.attr.textCheckMark;
+
+public class Send_ETA extends AppCompatActivity implements View.OnClickListener {
+    EditText dateEditText;
+    EditText timeEditText;
+    String date;
+    String time;
+    ImageView correctFormatIndicator;
+    private SimpleDateFormat dateFormat;
+    private DatePickerDialog datePicker;
+    private TimePickerDialog timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_eta);
+
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateEditText = (EditText) findViewById(R.id.editText2);
+        dateEditText.setInputType(InputType.TYPE_NULL);
+        //dateEditText.requestFocus();
+
+        timeEditText = (EditText) findViewById(R.id.editText);
+        timeEditText.setInputType(InputType.TYPE_NULL);
+
+        dateEditText.setOnClickListener(this);
+        timeEditText.setOnClickListener(this);
+        Calendar calendar = Calendar.getInstance();
+        datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newCalendar = Calendar.getInstance();
+                newCalendar.set(year,month,dayOfMonth);
+                dateEditText.setText(dateFormat.format(newCalendar.getTime()));
+            }
+
+        },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+        timePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                timeEditText.setText( selectedHour + ":" + selectedMinute);
+            }
+        }, calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), true);
     }
 
-    public void sendNewETA(View view) {
-        //TODO Kolla inputs så att de innehåller giltiga värden (Datum och tid)
-        //TODO kod för att uppdatera senast skickat ETA
-
-        EditText dateText = (EditText) findViewById(R.id.editText2);
-        EditText timeText = (EditText) findViewById(R.id.editText);
-        String date = dateText.getText().toString();
-        String time = timeText.getText().toString();
-
-
-        if(!isValidDate(date)){
-            dateText.setText("Invalid date");
-            dateText.setTextColor(Color.RED);
+    @Override
+    public void onClick(View v) {
+        if (v==dateEditText){
+            datePicker.show();
         }
-        if(!isValidTime(time)){
-            timeText.setText("Invalid time");
-            timeText.setTextColor(Color.RED);
+        else if(v==timeEditText){
+            timePicker.show();
         }
+
     }
-
-
-
-
-    public static boolean isValidDate(String date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        simpleDateFormat.setLenient(false);
-        try {
-            simpleDateFormat.parse(date.trim());
-        } catch (ParseException pe) {
-            return false;
-        }
-        return true;
-    }
-    
-    public static boolean isValidTime(String time) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
-        simpleDateFormat.setLenient(false);
-        try {
-            simpleDateFormat.parse(time.trim());
-        } catch (ParseException pe) {
-            return false;
-        }
-        return true;
-    }
-
 }

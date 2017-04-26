ea2d3f6951a706c736214b0fae7719bea3d86865
Max Edman <max.edman@gmail.com>
2017-04-26 13:16:10 +0200

    Visa kö på PCM

--- WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/ViewPCM.java
+++ WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/ViewPCM.java
@@ -4,15 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import java.util.UUID;
-
-import RESTServices.AMSS;
-import ServiceEntities.ArrivalLocation;
-import ServiceEntities.Location;
-import ServiceEntities.LocationState;
-import ServiceEntities.LocationType;
-import ServiceEntities.PortCallMessage;
-import ServiceEntities.Position;
+import RESTServices.MessageBrokerQueue;
 
 public class ViewPCM extends AppCompatActivity {
 
@@ -26,15 +18,15 @@ public class ViewPCM extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.textView2);
 //        String wrTest = portCdmCon.getLatestPortCalls(4);
 //        String wrTest = portCdmCon.pollQueueTest();
-//        MessageBrokerQueue msgBrokerQueue = new MessageBrokerQueue("19531434-b926-4a2a-aae0-949f8bec9442");
+        MessageBrokerQueue msgBrokerQueue = new MessageBrokerQueue("19531434-b926-4a2a-aae0-949f8bec9442");
 //        MessageBrokerQueue msgBrokerQueue = new MessageBrokerQueue();
 //        msgBrokerQueue.createUnfilteredQueue();
-//        msgBrokerQueue.pollQueue();
+        msgBrokerQueue.pollQueue();
 //        String wrTest = msgBrokerQueue.toString();
-//        String wrTest = msgBrokerQueue.toString();
-//        textView.setText(wrTest);
+        String wrTest = msgBrokerQueue.toString();
+        textView.setText(wrTest);
 
-        ArrivalLocation arrLoc = new ArrivalLocation(null, new Location(null, new Position(0,0, "Gothenburg"), LocationType.TRAFFIC_AREA));
+        /*ArrivalLocation arrLoc = new ArrivalLocation(null, new Location(null, new Position(0,0, "Gothenburg"), LocationType.TRAFFIC_AREA));
         LocationState locState = new LocationState("VESSEL", "2017-04-26T19:00:00.000Z", "ESTIMATED", arrLoc, null);
         PortCallMessage pcm = new PortCallMessage("urn:mrn:stm:vessel:IMO:9501368",
                                                   "urn:mrn:stm:portcdm:message:" + UUID.randomUUID().toString(),
@@ -42,7 +34,7 @@ public class ViewPCM extends AppCompatActivity {
                                                     locState);
         System.out.println(pcm.toXml());
         AMSS amss = new AMSS(pcm);
-        amss.submitStateUpdate();
+        amss.submitStateUpdate();*/
 
 
 

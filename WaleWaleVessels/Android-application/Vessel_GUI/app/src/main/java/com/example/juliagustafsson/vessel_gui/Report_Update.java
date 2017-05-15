a6a92c974f8b8e690d00259895b44f7ba0261430
ericjosefsson <ericjosefsson@Erics-MBP.lan>
2017-05-15 13:47:30 +0200

    setTimeAndDate lite förbättrad

--- WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/Report_Update.java
+++ WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/Report_Update.java
@@ -185,13 +185,14 @@ public class Report_Update extends AppCompatActivity implements View.OnClickList
         dateEditText.setOnClickListener(this);
         timeEditText.setOnClickListener(this);
         Calendar calendar = Calendar.getInstance();
+        timeEditText.requestFocus();
         datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
 
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 Calendar newCalendar = Calendar.getInstance();
                 newCalendar.set(year,month,dayOfMonth);
                 dateEditText.setText(dateFormat.format(newCalendar.getTime()));
-                timeEditText.requestFocus();
+
             }
         },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
 
@@ -199,6 +200,7 @@ public class Report_Update extends AppCompatActivity implements View.OnClickList
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                 timeEditText.setText( String.format("%02d", selectedHour) + ":" + String.format("%02d", selectedMinute));
+                dateEditText.requestFocus();
             }
         }, calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), true);
     }

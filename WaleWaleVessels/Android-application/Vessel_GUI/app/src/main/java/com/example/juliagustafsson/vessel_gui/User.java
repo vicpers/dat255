2d14d94b7febe443a9bd4f4da074344889c32b2f
ericjosefsson <ericjosefsson@Erics-MBP.lan>
2017-05-21 20:03:43 +0200

    Notifikationer ej klickbara!

--- WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/User.java
+++ WaleWaleVessels/Android-application/Vessel_GUI/app/src/main/java/com/example/juliagustafsson/vessel_gui/User.java
@@ -378,7 +378,10 @@ public class User implements Runnable {
         // Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent);
         PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(resultPendingIntent);
+
+
+        //mBuilder.setContentIntent(resultPendingIntent);
+        // Remove comment above to make notifications clickable, bugs regarding multiple threads follow...
 
         NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
 
